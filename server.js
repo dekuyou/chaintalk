9e918bcc443838c3fc2ec1165d7725fec7902f6c
OpenShift guest <dekuyou@gmail.com>
2014-03-23 07:49:55 -0400

    its test

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node Ninja\n');
+  res.end('yes! its a test!!')
 }).listen(80);
 
