efcad86993b70399a0a15a90ec21e3da12a14b7e
OpenShift guest <dekuyou@gmail.com>
2014-03-23 08:37:06 -0400

    c9 syupport

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var http = require('http');
+
+var port = process.env.PORT;
+
+if (port === undefined) {
+    port = 80;   
+}
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
   var response = 'Hello Node Ninja\n' + 'yes! its a test!!';
   
   res.end(response);
-}).listen(80);
+}).listen(port);
+
 
