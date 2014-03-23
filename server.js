19843a3d14f04ffaa1387e6b0e6cf80ae74c52a3
OpenShift guest <dekuyou@gmail.com>
2014-03-23 09:44:25 -0400

    Express

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var http = require('http');
+var express = require('express'),
+    app = express();
+
+app.use(express.logger());
 
 // cloud9 settings 
 var port = process.env.PORT;
@@ -7,20 +10,23 @@ if (port === undefined) {
     port = 80;   
 }
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  
-  var response = 'Hello Node Ninja\n' 
-  　+ 'yes! its a test!!' + '\n' 
-    + '' + '\n'
-    + '-cloud9 support' + '\n'
-    + '-auto sync ' + '\n'
-    + '- set a webhooks' + '\n'
-    + '日本語テスト';
-  
-  res.end(response);
-}).listen(port);
 
+app.get('/', function(req, res){
+    
+    var response = '<h1>Hello Node Ninja in Express</h1>' 
+  　    + 'yes! its a test!!' 
+        + '<ul>' 
+        + '<li>cloud9 support</li>'
+        + '<li>auto sync</li>'
+        + '<li>set a webhooks</li>'
+        + '<li>日本語テスト</li>'
+        + '</ul>';
+    
+    res.send(response);
+});
+
+app.listen(port);
+console.log('Express server started on port %s', process.env.PORT);
 
 // auto sync test
 
