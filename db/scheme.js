91817fd10d38b69f4196da8f6e9fc5b3791f3cc8
OpenShift guest <dekuyou@gmail.com>
2014-03-25 05:03:45 -0400

    Arrangement

--- db/scheme.js
+++ db/scheme.js
@@ -2,33 +2,46 @@ module.exports.mongoose            = require('mongoose');
 module.exports.Schema;
 
 
+/**
+ * DB 初期化
+ * ここにcollectionを書いていく
+ */
 exports.init = function(){
     
     
-this.Schema = this.mongoose.Schema;
+    this.Schema = this.mongoose.Schema;
 
-var ChatTestSchema = new this.Schema({
-  message: String,
-  date: Date
-});
-this.mongoose.model('ChatTest', ChatTestSchema);
-this.mongoose.connect('mongodb://'+ (process.env.IP || 'localhost') +'/chat_app');
+    // ChatTest collection for test -------------------------------------------
+    var ChatTestSchema = new this.Schema({
+      message: String,
+      date: Date
+    });
+    this.mongoose.model('ChatTest', ChatTestSchema);
+    
+    // User collection -------------------------------------------------------
+    var UserSchema = new this.Schema({
+      user_id:  String,
+      sns_type: Number,
+      pub_key: String,
+      created_at: Date
+    });
+    this.mongoose.model('User', UserSchema);
+    
+    // Messages collection ----------------------------------------------------
+    var MessageSchema = new this.Schema({
+       user_id: String,
+       message: String,
+       geolocation: String,
+       created_at: Date
+    });
+    this.mongoose.model('Message', MessageSchema);
+
     
     
     
-}
+    this.mongoose.connect('mongodb://'+ (process.env.IP || 'localhost') +'/chat_app');
+    
+    
+    
+};
 
-/*
-module.exports.UserSchema = new module.exports.Schema({
-  user_id: String,
-  sns_type: Number,
-  pub_key: String,
-  date: Date
-});
-
-module.exports.MessagesSchema = new module.exports.Schema({
-   user_id: String,
-   message: String,
-});
-
-*/
